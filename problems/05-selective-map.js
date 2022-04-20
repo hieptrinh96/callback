6202f434a5891f3c203fa0b35586f449fec82bc0
nate <nscott1010@gmail.com>
2022-04-20 15:07:03 -0500

    message

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -10,6 +10,23 @@ problem.
 
 Examples:
 
+
+
+AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
+    (optional if you already asked a question for this problem.)
+- Explain how you are using both of the callbacks in the function.
+- What do you expect each callback function to be returning?
+- How many times are you calling each callback function?
+*******************************************************************************/
+
+let selectiveMap = function() {
+    
+};
+
+
+
+
+
 function isEven(n) {
     return n % 2 === 0;
 }
@@ -34,22 +51,5 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
 
 console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 // [-10, 16, 49, 36, -2, -9]
-
-AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
-    (optional if you already asked a question for this problem.)
-- Explain how you are using both of the callbacks in the function.
-- What do you expect each callback function to be returning?
-- How many times are you calling each callback function?
-*******************************************************************************/
-
-let selectiveMap = function() {
-
-};
-
-
-
-
-
-
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = selectiveMap;
